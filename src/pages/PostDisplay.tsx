ec8e766b7effcd7f740e7d4b1085d5b8fef1280b
Evan Bridgman-Fralin <fralinev@gmail.com>
2023-06-23 12:18:22 -0400

    minor

--- src/pages/PostDisplay.tsx
+++ src/pages/PostDisplay.tsx
@@ -1,6 +1,9 @@
 import Post from "./Post";
 
 const PostDisplay = ({ posts }: any) => {
+  if (!posts) {
+    return <div>loading...</div>;
+  }
   return (
     <>
       <div className="post-display-outer-container">
