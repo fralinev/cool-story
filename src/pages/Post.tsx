b5064dadda9ca02142423e840d10bb68f1b67c4c
Evan Bridgman-Fralin <fralinev@gmail.com>
2023-07-06 12:23:46 -0400

    minor

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -5,10 +5,10 @@ const Post = ({ post, setPosts }: any) => {
   const [hovered, setHovered] = useState(-1);
 
   const deletePost = async (id: any) => {
-    const response = await axios.delete(`/api/posts/${id}`);
-    setPosts((prev: any) => {
-      return prev.filter((post: any) => post._id !== id);
-    });
+    // const response = await axios.delete(`/api/posts/${id}`);
+    // setPosts((prev: any) => {
+    //   return prev.filter((post: any) => post._id !== id);
+    // });
   };
 
   const deleteButton = (
@@ -17,7 +17,7 @@ const Post = ({ post, setPosts }: any) => {
 
   return (
     <div
-      onMouseEnter={() => setHovered(post._id)}
+      onMouseEnter={() => setHovered(post ? post._id : null)}
       onMouseLeave={() => setHovered(-1)}
     >
       <div className="post">
