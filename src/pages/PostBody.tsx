0669b1fc4217f6611c62bad94bcf0df85fd9dca2
Evan Bridgman-Fralin <fralinev@gmail.com>
2023-07-07 20:19:32 -0400

    minor styling

--- src/pages/PostBody.tsx
+++ src/pages/PostBody.tsx
@@ -1,6 +1,9 @@
 import styles from "./PostBody.module.css";
 
 const PostBody = ({ post }: any) => {
+  if (!post) {
+    return <div>loading...</div>;
+  }
   return (
     <div>
       <p className={styles.postBody}>{post.body}</p>
